379f3530e476ac631dee85f37360af12242e5ea9
Priyank Patel <priyank89patel@gmail.com>
2020-05-07 21:40:07 -0400

    initial commit

---
+++ src/Identity_Sample/Identity_Sample/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity_Sample.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
---
+++ src/Identity_Sample/Identity_Sample/Services/EmailSender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Identity_Sample.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public AuthMessageSenderOptions Options { get; }
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var response = await Execute(Options.SendGridKey, subject, message, email);
+
+            if (response.StatusCode != System.Net.HttpStatusCode.Accepted)
+            {
+                // log or throw
+                throw new Exception("Could not send email: " + await response.Body.ReadAsStringAsync());
+            }
+        }
+
+        private async Task<Response> Execute(string apiKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("priyank89patel@gmail.com",Options.SendGridUser),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+
+            return await client.SendEmailAsync(msg);
+        }
+    }
+}
