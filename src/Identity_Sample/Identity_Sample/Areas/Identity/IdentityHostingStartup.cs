5e4241fe34d6820d90b5639dbc49e2cf644071a7
Priyank Patel <priyank89patel@gmail.com>
2020-05-10 19:53:51 -0400

    Scaffold additional identity pages

---
+++ src/Identity_Sample/Identity_Sample/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Identity_Sample.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Identity_Sample.Areas.Identity.IdentityHostingStartup))]
+namespace Identity_Sample.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
